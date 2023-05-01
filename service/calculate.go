c6ad8c488a28de62a2157f59618303944c4a459d
sinqx <sinqxpls@gmail.com>
2023-05-01 16:27:42 +0600

    optimize calculation

--- service/calculate.go
+++ service/calculate.go
@@ -1,39 +1,58 @@
 package service
 
 import (
+	"runtime"
 	"sync"
 	pb "tt/pkg/api"
 )
 
 func Calculate(numbers []*pb.Numbers) int64 {
+	var sum int64
 	var wg sync.WaitGroup
+	var mu sync.Mutex
 
-	input := make(chan int)
-	results := make(chan int)
+	numGoroutines := runtime.NumCPU() //количество горутин
 
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		for _, num := range numbers {
-			input <- int(num.Nums)
+	chunkSize := len(numbers) / numGoroutines
+	chunks := make([][]*pb.Numbers, numGoroutines)
+
+	for i := 0; i < numGoroutines; i++ {
+		start := i * chunkSize
+		end := start + chunkSize
+
+		if i == numGoroutines-1 {
+			end = len(numbers)
 		}
-		close(input)
-	}()
+		chunks[i] = numbers[start:end]
+	}
 
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		var sum int
-		for n := range input {
-			sum += n
-		}
-		results <- sum
-	}()
+	// Запустить горутины
+	for i := 0; i < numGoroutines; i++ {
+		wg.Add(1)
+		go func(nums []*pb.Numbers) {
+			defer wg.Done()
 
-	go func() {
-		wg.Wait()
-		close(results)
-	}()
+			var localSum int64
 
-	return int64(<-results)
+			for _, num := range nums {
+				localSum += num.Nums
+			}
+
+			mu.Lock()
+			sum += localSum
+			mu.Unlock()
+
+		}(chunks[i])
+	}
+
+	wg.Wait()
+	return sum
 }
+
+//func Calculate(numbers []*pb.Numbers) int64 {
+//	var sum int64 = 0
+//	for _, num := range numbers {
+//		sum += num.Nums
+//	}
+//	return sum
+//}
